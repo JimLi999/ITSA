c30e4b4800596e5b11936fefda0dc3dfcee0f7c4
JimLi999 <jam9112229@gmail.com>
2023-03-16 05:55:40 +0800

    upload via Github Desktop
    Add solution of ITSA Problem 7

---
+++ exercise230315/exercise_7_230315/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //process inputs.
+        int month = sc.nextInt();
+        logic(month);    //start logic process.
+    }
+
+    private static void logic(int month) {
+        System.out.println(calculateWorms(month));
+    }
+
+    private static int calculateWorms(int month){
+        if(month<=4)
+            return 1;
+        else
+            return calculateWorms(month-3)+calculateWorms(month-2);
+    }
+
+}
