94a0f9f139e658258058849077ed7500543eb6b5
JimLi999 <62498401+JimLi999@users.noreply.github.com>
2023-03-16 06:03:21 +0800

    Update Main.java

--- exercise230315/exercise_7_230315/src/Main.java
+++ exercise230315/exercise_7_230315/src/Main.java
@@ -12,11 +12,11 @@ public class Main {
         System.out.println(calculateBabies(month));
     }
 
-    private static int calculateWorms(int month){
+    private static int calculateBabies(int month){
         if(month<=4)
             return 1;
         else
-            return calculateWorms(month-3)+calculateWorms(month-2);
+            return calculateBabies(month-3)+calculateBabies(month-2);
     }
 
 }
