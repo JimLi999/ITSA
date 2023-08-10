8b063a41f338916366f5b6e22da067b1c11790b4
JimLi999 <jam9112229@gmail.com>
2023-08-10 16:13:26 +0800

    Update Main.java

--- practices/C_MM04_E/src/Main.java
+++ practices/C_MM04_E/src/Main.java
@@ -15,19 +15,27 @@ public class Main {
                 System.out.println(rawData[0] + "+" + rawData[1] + "=" + calcAdd(rawData));
                 System.out.println(rawData[0] + "*" + rawData[1] + "=" + calcMulti(rawData));
                 System.out.println(rawData[0] + "-" + rawData[1] + "=" + calcSub(rawData));
-                System.out.println(rawData[0] + "/" + rawData[1] + "=" + calcDiv(rawData) + "..." + calcDivRemind(rawData));
+                int[] divResult=calcDiv(rawData);
+                System.out.println(rawData[0] + "/" + rawData[1] + "=" + divResult[0] + "..." + divResult[1]);
                 //reset the index.
                 i = 0;
             }
         }
     }
 
-    private static int calcDivRemind(int[] rawData) {
-        return rawData[0] % rawData[1];
-    }
+    private static int[] calcDiv(int[] rawData) {
+        int quotient = rawData[0] / rawData[1];
+        int reminder = rawData[0] % rawData[1];
+        if(reminder<0){
+            if(quotient>0){
+                quotient++;
+            }else
+                quotient--;
 
-    private static int calcDiv(int[] rawData) {
-        return rawData[0] / rawData[1];
+            reminder=rawData[0]- rawData[1] * quotient;
+        }
+
+        return new int[]{quotient,reminder};
     }
 
     private static int calcSub(int[] rawData) {
