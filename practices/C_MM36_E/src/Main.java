b32c6658e891e4b6dfee6b73fdc0ccc04f677746
JimLi999 <jam9112229@gmail.com>
2024-06-03 19:34:36 +0800

    Upload C_MM36_E via GitHub Desktop

---
+++ practices/C_MM36_E/src/Main.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //variable
+        int rawData;
+
+        //process inputs.
+        while (sc.hasNextInt()) {
+            rawData = sc.nextInt();
+            System.out.println(CheckSeason(rawData));
+        }
+    }
+
+    private static String CheckSeason(int month) {
+        switch (month){
+            case 3:
+            case 4:
+            case 5:
+                return "Spring";
+            case 6:
+            case 7:
+            case 8:
+                return "Summer";
+            case 9:
+            case 10:
+            case 11:
+                return "Autumn";
+            default:
+                return "Winter";
+        }
+    }
+
+}
